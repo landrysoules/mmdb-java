c0d19ab0aba75ece81043cbfed5e42014a65929a
Landry Soules <landry@spaceinvade.rs>
2017-10-24 21:05:42 +0200

    Fix pom and mongo config

--- src/main/java/rs/spaceinvade/mmdbjava/MongoConfig.java
+++ src/main/java/rs/spaceinvade/mmdbjava/MongoConfig.java
@@ -1,5 +1,7 @@
 package rs.spaceinvade.mmdbjava;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,8 +18,9 @@ public class MongoConfig{
 	
 	@Value("${mongodb.port}")
 	private int mongodbPort;
-
+	protected Logger logger = LoggerFactory.getLogger(this.getClass());
   public @Bean MongoDbFactory mongoDbFactory() {
+	  logger.warn("******* " + mongodbHost + ":" + mongodbPort);
     return new SimpleMongoDbFactory(new MongoClient(mongodbHost, mongodbPort), "mmdb_java");
   }
 }
